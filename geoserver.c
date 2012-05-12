87d2379f02b1fbfb7ac9a568d1f36c09a205c59f
David Nathan <20356245@student.uwa.edu.au>
2012-05-12 15:42:13 +0800

    update

--- geoserver.c
+++ geoserver.c
@@ -7,10 +7,73 @@
 #include <arpa/inet.h>   // for sockaddr_in and inet_addr()
 #include <netinet/in.h>
 #include "ipod3230.h"
+#define _POSIX_C_SOURCE 1
 
 static const int MAXPENDING = 5; //Max. outstanding connection requests
 
-void geoAppProtocol(int clnSocket) {
+void DieWithUserMessage(const char *msg, const char *detail) {
+    fputs(msg, stderr);
+    fputs(": ",stderr);
+    fputs(detail, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+void DieWithSystemMessage(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int SetupTCPServerSocket(unsigned short port){
+    
+    //Create socket for incoming connections
+    int sock;//Socket descriptor for server
+    if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))< 0)
+        DieWithSystemMessage("socket() failed");
+    
+    //Construct local address structure
+    struct sockaddr_in servAddr;                   //Local address
+    memset(&servAddr, 0, sizeof(servAddr));        //Zero out structure
+    servAddr.sin_family = AF_INET;                 //IPv4 address family
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);  //Any incoming interface
+    servAddr.sin_port = htons(port);               //Local port
+    
+    //Bind to the local address
+    if(bind(sock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
+        DieWithSystemMessage("bind() failed");
+    
+    //Mark the socked so it will listen for incoming conncetions
+    if(listen(sock, MAXPENDING) < 0)
+        DieWithSystemMessage("listen() failed");
+    
+    return sock;
+}
+
+int AcceptTCPConnection(int servSock) {
+   
+    struct sockaddr_in clntAddr; //Client address
+    //Set length of client address structure (in-out parameter)
+    socklen_t clntAddrLen = sizeof(clntAddr);
+    
+    //Wait for a client to connect
+    int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntAddrLen);
+    if(clntSock < 0)
+        DieWithSystemMessage("accept() failed");
+    
+    //clntSock is now connected to a client
+    
+    char clntName[INET_ADDRSTRLEN]; //String to contain client address
+    if(inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr,clntName, sizeof(clntName)) != NULL)
+        printf("Handling client %s/%d\n", clntName, ntohs(clntAddr.sin_port));
+    else
+        puts("Unable to get client address");
+    
+    return clntSock;
+
+}
+
+
+void geoAppProtocol(int clntSocket) {
   
     /* 
     char buffer[BUFSIZE]; //Buffer for IPOD request 
@@ -31,41 +94,31 @@ void geoAppProtocol(int clnSocket) {
     
     int8_t numSamples;
     
-    ssize_t numBytesRcvd = recv(clntSocket,numSamples,1, 0);
+    ssize_t numBytesRcvd = recv(clntSocket,(void*)(&numSamples),1, 0);
     if(numBytesRcvd < 0)
         DieWithSystemMessage("recv() failed");
     
     printf("Number of Samples to receive: %d\n", numSamples);
 
+    
+    IPOD_SAMPLE samples[numSamples]; 
+    //wrap the socket in an input stream
     FILE *instream = fdopen(clntSocket, "r");
-    if(channel == NULL)
+    if(instream == NULL)
         DieWithSystemMessage("fdopen() failed");
     
-    SAMPLE_IPOD samples[numSamples];    
+    for(int i=0; i<numSamples; i++){
+        if(fread(&samples[i], sizeof(IPOD_SAMPLE), 1, instream) != 1)
+            DieWithSystemMessage("fread() failed");
+    }
     
+    fclose(instream); //close the socket connection
     
-    
+    //TODO: Collate and Calculate (lat, lon)
     
     
 }
 
-
-
-
-
-
-void DieWithUserMessage(const char *msg, const char *detail) {
-    fputs(msg, stderr);
-    fputs(": ",stderr);
-    fputs(detail, stderr);
-    fputc('\n', stderr);
-    exit(1);
-}
-
-void DieWithSystemMessage(const char *msg) {
-    perror(msg);
-    exit(1);
-}
 
 int main(int argc, char *argv[]){
     
@@ -74,45 +127,12 @@ int main(int argc, char *argv[]){
     
     in_port_t servPort = atoi(argv[1]); //First arg: local port
     
-    //Create socket for incoming connections
-    int servSock;//Socket descriptor for server
-    if((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))< 0)
-        DieWithSystemMessage("socket() failed");
-    
-    //Construct local address structure
-    struct = sockaddr_in servAddr;                 //Local address
-    memset(&servAddr, 0, sizeof(servAddr));        //Zero out structure
-    servAddr.sin_family = AF_INET;                 //IPv4 address family
-    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);  //Any incoming interface
-    servAddr.sin_port = htons(servPort);           //Local port
-    
-    //Bind to the local address
-    if(bind(servSock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
-        DieWithSystemMessage("bind() failed");
-    
-    //Mark the socked so it will listen for incoming conncetions
-    if(listen(servSock, MAXPENDING) < 0)
-        DieWithSystemMessage("listen() failed");
-    
-    while(1) { //Run forever
-        struct sockaddr_in clntAddr; //Client address
-        //Set length of client address structure (in-out parameter)
-        socklen_t clntAddrLen = sizeof(clntAddr);
+    int servSock = SetupTCPServerSocket(servPort);
         
-        //Wait for a client to connect
-        int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntAddrLen);
-        if(clntSock < 0)
-            DieWithSystemMessage("accept() failed");
-        
-        //clntSock is now connected to a client
-        
-        char clntName[INET_ADDRSTRLEN]; //String to contain client address
-        if(inet_ntop(AF_INET, &clntAddr.sin_addr.s_addr,clntName, sizeof(clntName)) != NULL)
-            printf("Handling client %s/%d\n", clntName, ntohs(clntAddr.sin_port));
-        else
-            puts("Unable to get client address");
-        
+    while(1) {        
+        int clntSock = AcceptTCPConnection(servSock);
         geoAppProtocol(clntSock);
+        
     }
     
     //NOT REACHED
